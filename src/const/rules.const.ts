4d7586bdd7af88137044a9874d185c623a855b72
Justin Huertas <justin.hv08@gmail.com>
2025-07-26 20:21:40 -0500

    :sparkles: feat: Create rules view, added tabs to switch the display between general rules and group rules and added an alert banner

---
+++ src/const/rules.const.ts
@@ -0,0 +1,62 @@
+export const COMMUNITY_RULES_DISCORD = [
+	{
+		title: "🤝✨ Respeto",
+		description: "Trata a todos los miembros con amabilidad y respeto. No toleramos el acoso (ni dentro ni fuera de la comunidad), la discriminación, los insultos o cualquier forma de comportamiento ofensivo."
+	},
+	{
+		title: "📚📌 Usa los canales adecuados",
+		description: "Utiliza los canales temáticos correspondientes para cada tipo de discusión o pregunta. Esto ayudará a mantener el orden y facilitará la búsqueda de información relevante."
+	},
+	{
+		title: "🚫📵 Contenido",
+		description: "No compartas contenido ofensivo, violento, pornográfico o ilegal. Mantén el ambiente seguro y adecuado para todos los miembros."
+	},
+	{
+		title: "🧠🫱🏼‍🫲🏻 Actitud",
+		description: "No todos los miembros tienen el mismo nivel de conocimiento o experiencia. Sé paciente al responder preguntas y ofrece apoyo a aquellos que están aprendiendo."
+	},
+	{
+		title: "🔒🙈 Privacidad",
+		description: "No compartas información personal de otros miembros sin su consentimiento. Protege tu propia privacidad también. No compartas información confidencial en los chats públicos."
+	},
+	{
+		title: "🚨🛡️ Reporta",
+		description: "Si encuentras comportamientos inapropiados, incumplimientos de las reglas o cualquier otro problema, informa a los admins para que puedan tomar medidas adecuadas. Esto incluye el acoso en privado por parte de miembros de la comunidad."
+	},
+	{
+		title: "⛔🗣️ Discusiones",
+		description: "Evita discusiones políticas y religiosas. Para mantener un ambiente neutral y respetuoso, evita discutir temas políticos o religiosos, ya que pueden generar conflictos y división en la comunidad."
+	},
+	{
+		title: "🚀💬 Proactividad",
+		description: "Anima a los miembros a participar, compartir sus conocimientos y contribuir a la comunidad. La participación activa enriquece la experiencia de todos."
+	},
+	{
+		title: "🧑‍⚖️📣 Respeta a los admins",
+		description: "Si un administrador te pide que lleves cierto tema al grupo correspondiente, o te indica una instrucción a seguir, por favor no lo discutas o si tienes alguna objeción, preséntala amablemente. Valoramos tu cooperación."
+	}
+];
+
+
+export const COMMUNITY_RULES_GROUPS = [
+	{
+		title: "👋🏻 🗣 General",
+		description: "Si bien este espacio es para charlar de cualquier tema que no entre en los demás grupos, existen ciertos temas que no pueden tocarse, sin excepción, como por ejemplo: religión, política, contenido erótico o insinuante."
+	},
+	{
+		title: "📚 🆘 Recursos y Ayuda",
+		description: "No se puede utilizar el grupo para ofrecer ayuda/mentoría a cambio de dinero. Es mejor definir ese tipo de situaciones por privado. Sin embargo, esto no quiere decir que se esté dando permiso a que se pueda acosar a miembros de la comunidad por privado para ofrecer servicios. Si otro miembro de la comunidad te acosa por privado, por favor comunícate con un administrador (y acompañado con pruebas)."
+	},
+	{
+		title: "🌎📝 Networking",
+		description: "No está permitido compartir eventos de otras comunidades de WhatsApp sin previa autorización de los administradores. Tampoco se permite el spam de publicaciones, medios o servicios propios que no estén estrechamente relacionados con contenido IT. En caso de querer ofrecer un servicio IT, por favor consultar a los administradores primero."
+	},
+	{
+		title: "☕💧 La Llorería",
+		description: "A pesar de ser un espacio para desahogarnos y compartir nuestros problemas, por favor no te excedas y no envíes constantemente mensajes hablando solamente de tus problemas (o del mismo problema) de manera incesante. Se espera que también puedas contribuir, escuchar y aconsejar a otros."
+	},
+	{
+		title: "💼🤝 Ofertas Laborales",
+		description: "Tal como lo indica el título y descripción del grupo, no está permitido debatir/opinar/consultar sobre las ofertas que se comparten. El grupo es solamente para compartir ofertas y nada más. De ese modo se evita que el grupo sea difícil de leer."
+	}
+];
